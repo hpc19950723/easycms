3fc0592f63a73e7865114d2b540eab349e3aa7f4
annronwoo <aaronwoodream@gmail.com>
2016-06-04 22:02:23 +0800

    remove all

--- modules/admin/Module.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\modules\admin;
-
-class Module extends \app\modules\CommonModule
-{
-    public $controllerNamespace = 'app\modules\admin\controllers';
-}
--- modules/user/admin/Module.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\modules\user\admin;
-
-class Module extends \app\modules\CommonModule
-{
-    public $controllerNamespace = 'app\modules\user\admin\controllers';
-}
